8b37d5c82272ae1e70286e00278c151377053b59
driss-chnc <chaneac.driss@gmail.com>
2026-06-28 18:20:18 +0200

    Low Program

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers from 0 to 14 ten times
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
