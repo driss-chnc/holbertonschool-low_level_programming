a9131eaa3f1cf423ed4199db17433b23bc1a488b
driss-chnc <chaneac.driss@gmail.com>
2026-07-01 10:22:26 +0200

    Low Program

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * 
- * reset to 98 - This function changes the pointed value to 98.
- * 
- * 
+ * reset_to_98 - This function changes the pointed value to 98.
+ * @n: pointer
  */
-
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
