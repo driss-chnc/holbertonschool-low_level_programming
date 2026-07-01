90bd9801f50031c5e5baf054537734a85de3d8fc
driss-chnc <chaneac.driss@gmail.com>
2026-07-01 13:29:13 +0200

    Low Program

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+	int len = 0;
+	int i;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
 	for (i = len - 1; i >= 0; i--)
 	{
