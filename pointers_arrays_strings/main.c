2f54dea834bdbb1c68b91672be75a4e312e7503d
driss-chnc <chaneac.driss@gmail.com>
2026-07-01 13:07:22 +0200

    Low Program

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,11 +7,9 @@
  */
 int main(void)
 {
-	char *str;
-	int len;
+    char *str;
 
-	str = "My first strlen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
