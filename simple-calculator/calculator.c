4c67ab81dbe85fe493b4c4de91114e8c1421e2b0
driss-chnc <chaneac.driss@gmail.com>
2026-07-03 17:29:41 +0200

    Calculatrice

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,22 +1,69 @@
 #include <stdio.h>
 int main (void)
 {
-	int choice;
-	/** 
-	int A = 0;
-	int B = 0;
+	int choice = 1;
+	int a = 0;
+	int b = 0;
 	int result = 0;
-	*/
 
 	printf ("Simple Calculator\n");
+
 	while (choice != 0)
 		{
+			if (choice < 5 && choice > -1)
+			{
 			printf ("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
+			}
 			printf ("choice ?\n");
 			scanf ("%d",&choice);
+			if (choice < 0 || choice > 4)
+			{
+				printf ("Invalid Choice!\n");
+			}
 			if (choice == 0)
 			{
 				printf ("Bye!");
+			}
+			if (choice == 1)
+			{
+				printf ("A: ");
+				scanf ("%d", &a);
+				printf ("B: ");
+				scanf ("%d", &b);
+				result = a + b;
+				printf ("Result = %d\n", result);
+			}
+			if (choice == 2)
+			{
+				printf ("A: ");
+				scanf ("%d", &a);
+				printf ("B: ");
+				scanf ("%d", &b);
+				result = a - b;
+				printf ("Result = %d\n", result);
+			}
+			if (choice == 3)
+			{
+				printf ("A: ");
+				scanf ("%d", &a);
+				printf ("B: ");
+				scanf ("%d", &b);
+				result = a * b;
+				printf ("Result = %d\n", result);
+			}
+			if (choice == 4)
+			{
+				printf ("A: ");
+				scanf ("%d", &a);
+				printf ("B: ");
+				scanf ("%d", &b);
+				if (b == 0)
+				{
+					printf ("Error: division by zero\n");
+					continue;
+				}
+				result = a / b;
+				printf ("Result = %d\n", result);
 			}
 
 		}
