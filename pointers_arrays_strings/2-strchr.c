a88876716266d62907849dfc0b141252c2287c37
driss-chnc <chaneac.driss@gmail.com>
2026-07-03 13:06:13 +0200

    Low Program

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to locate
+ *
+ * Return: a pointer to the first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
