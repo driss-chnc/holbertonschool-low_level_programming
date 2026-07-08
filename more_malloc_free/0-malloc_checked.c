c9e2711772dd4bf2e2c76017366cf081f114aa80
driss-chnc <chaneac.driss@gmail.com>
2026-07-08 10:12:09 +0200

    More malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: pointer to allocated memory, or exit with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int *array_range(int min, int max);
+void *malloc_checked(unsigned int b);
 
 #endif
