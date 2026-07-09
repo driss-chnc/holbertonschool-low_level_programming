e53200b010a672b5332a1c19a6442750712b795d
driss-chnc <chaneac.driss@gmail.com>
2026-07-09 13:32:34 +0200

    Dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner );
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
