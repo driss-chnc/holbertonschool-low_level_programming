c3283c8b935efd215609c2e2c548067f26e428dd
driss-chnc <chaneac.driss@gmail.com>
2026-07-30 11:21:24 +0200

    mod hash_tables.h main.c add 6-hash_table_delete.c

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *next;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
