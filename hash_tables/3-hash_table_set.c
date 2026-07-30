223dcf1a962f0c431123cc35e1e4d294fd02e40c
driss-chnc <chaneac.driss@gmail.com>
2026-07-30 10:52:26 +0200

    modhash_tables.h main.c add 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,57 +1,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
-
-
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
  * @key: key
  * @value: value
- *
  * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	hash_node_t *current;
+	hash_node_t *new, *tmp;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key || *key == '\0')
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-
-	current = ht->array[index];
-
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
+	for (tmp = ht->array[index]; tmp; tmp = tmp->next)
+		if (!strcmp(tmp->key, key))
 		{
-			free(current->value);
-			current->value = strdup(value);
-			return (1);
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (tmp->value ? 1 : 0);
 		}
-		current = current->next;
-	}
-
 	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	if (!new)
 		return (0);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
-
-	if (new->key == NULL || new->value == NULL)
+	if (!new->key || !new->value)
 	{
-		free(new->key);
-		free(new->value);
-		free(new);
+		free(new->key), free(new->value), free(new);
 		return (0);
 	}
-
 	new->next = ht->array[index];
 	ht->array[index] = new;
-
 	return (1);
 }
