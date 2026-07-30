295188c80118ef0dc057bacbd04d76169ae7c44a
driss-chnc <chaneac.driss@gmail.com>
2026-07-30 10:11:58 +0200

    mod hash_tables.c main.c add 2-key_index.c

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key
+ * @key: the key
+ * @size: size of the hash table
+ *
+ * Return: index where the key should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key,
+	unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
